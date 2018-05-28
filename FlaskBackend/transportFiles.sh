ad3dfbdca2169b7375d23ee1b5af21ef7f3e1e60
Andrew Luo <andrew.luo1@outlook.com>
2018-05-28 14:06:42 -0400

    added proper server code

---
+++ FlaskBackend/transportFiles.sh
@@ -0,0 +1,5 @@
+scp -i ~/Desktop/NLPSentiment.pem ~/Desktop/insight.ai/NLP/3-layer-NN-with-emotion-file/model.ckpt.data-00000-of-00001  ec2-user@ec2-184-72-144-143.compute-1.amazonaws.com:~/models
+scp -i ~/Desktop/NLPSentiment.pem ~/Desktop/insight.ai/NLP/3-layer-NN-with-emotion-file/model.ckpt.index  ec2-user@ec2-184-72-144-143.compute-1.amazonaws.com:~/models
+scp -i ~/Desktop/NLPSentiment.pem ~/Desktop/insight.ai/NLP/3-layer-NN-with-emotion-file/model.ckpt.index.tempstate14900430033068717499  ec2-user@ec2-184-72-144-143.compute-1.amazonaws.com:~/models
+scp -i ~/Desktop/NLPSentiment.pem ~/Desktop/insight.ai/NLP/3-layer-NN-with-emotion-file/model.ckpt.meta  ec2-user@ec2-184-72-144-143.compute-1.amazonaws.com:~/models
+scp -i ~/Desktop/NLPSentiment.pem ~/Desktop/insight.ai/NLP/3-layer-NN-with-emotion-file/lexicon.pickle  ec2-user@ec2-184-72-144-143.compute-1.amazonaws.com:~/models
